b7a6d6c3b0d1f6c0e061c3ab094b47bbf9451597
Sahilyadav24 <sahilrao2424@gmail.com>
2025-05-06 01:30:35 +0530

    Some More changes

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, World from Jenkins!.Testing is under Progress .'
+    return 'Hello, World from Jenkins!.Testing is under Progress..'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
